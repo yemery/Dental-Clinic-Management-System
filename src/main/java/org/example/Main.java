b2082f2861407dd60acdb7208bbf4c7b909cae31
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2025-01-28 17:08:52 +0100

    ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
+
+
 import org.example.presentation.view.frames.Auth;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         SwingUtilities.invokeLater(Auth::new);
     }
 }
